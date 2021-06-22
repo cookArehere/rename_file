5254141654d2a2535d49bd8e264360a4d6e78054
cookArehere <kunpanpavel@gmail.com>
2021-06-22 20:05:45 +0300

    Ошибка файлов с одинаковым именем ловится блоком try except, файлы сохраняются с индексом. Путь к пдф и папкам теперь вне директории програмы.

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ from pathlib import *
 from PyPDF2 import PdfFileReader
 import re
 
+def renaming_pdf_file(path, dir, name):
+
+    try:
+        path_name_pdf = Path(path, dir, f"{name}.pdf")
+        i_file.rename(path_name_pdf)
+
+    except FileExistsError:
+        order_list_same_file = list(Path(path, dir).glob("*.pdf"))
+
+        quantity = 0
+
+        for i in order_list_same_file:
+            if re.findall(name, str(i)):
+                quantity += 1
+
+        path_name_pdf = Path(path, dir, f"{name}_{int(quantity) + 1}.pdf")
+        i_file.rename(path_name_pdf)
+
 
 if __name__ == '__main__':
 
@@ -14,17 +32,14 @@ if __name__ == '__main__':
        page = pdf_file.getPage(0)
        text = page.extractText()
 
-       name_pdf = re.search(r"[S]?\w-\d\d\d[G]?", text)
+       name_pdf = re.search(r"[S]?\w-\d\d\d[G, AG, BG]?", text)
 
        if "E" in name_pdf[0]:
-           path_name_pdf = Path(path_for_pdf_file, "ERECTION PLATE", f"{name_pdf[0]}.pdf")
-           i_file.rename(path_name_pdf)
-       elif "SP" in name_pdf[0]:
-           path_name_pdf = Path(path_for_pdf_file, "SPESIAL PLATE", f"{name_pdf[0]}.pdf")
-           i_file.rename(path_name_pdf)
+           renaming_pdf_file(path_for_pdf_file, "ERECTION PLATE", name_pdf[0])
+       elif "SP" in name_pdf[0] or "Y" in name_pdf[0]:
+           renaming_pdf_file(path_for_pdf_file, "SPECIAL PLATE", name_pdf[0])
        elif "P" in name_pdf[0]:
-           path_name_pdf = Path(path_for_pdf_file, "STANDART PLATE", f"{name_pdf[0]}.pdf")
-           i_file.rename(path_name_pdf)
+           renaming_pdf_file(path_for_pdf_file, "STANDART PLATE", name_pdf[0])
 
        print("DONE", name_pdf[0])
 
